be0aa5b10067c7116037d2b49bb5de7c4bf37686
Diana Ilisei <dianailisei@gmail.com>
2018-05-20 11:17:43 +0300

    deleteUserApp

--- includes/deleteUserApp.inc.php
+++ includes/deleteUserApp.inc.php
@@ -1,2 +1,38 @@
-<? echo $_GET['id'];
+<?php 
+if(isset($_GET["id"])){
+    include("db.inc.php");
+    $id = $_GET["id"];
+    $query = "SELECT id FROM apps WHERE name = (SELECT name FROM apps WHERE id = $id) AND uploader = (SELECT uploader FROM apps WHERE id = $id)";
+    
+    $result = mysqli_query($conn, $query);
+    if(!$result) {
+        die("Query FAILED.<br>" . mysqli_error($conn)); 
+        exit();
+    }
+    else
+    {
+        while($row = mysqli_fetch_assoc($result)){
+            $query = "DELETE FROM apps WHERE id = " . $row["id"];
+            $deleteResult = mysqli_query($conn, $query);
+            if(!$deleteResult) {
+                die("Query FAILED.<br>" . mysqli_error($conn)); 
+                exit();
+            }
+        }
+        $query = "DELETE FROM apps WHERE id = $id";
+        $result = mysqli_query($conn, $query);
+        if(!$result) {
+            die("Query FAILED.<br>" . mysqli_error($conn)); 
+            exit();
+        }
+        else {
+            header("Location: ../profile.php");
+            exit();
+        }
+    }
+}
+else {
+    header("Location: ../profile.php");
+    exit();
+}
 ?>
